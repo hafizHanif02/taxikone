e4fb99b081724925a0567c03189dfed6dd03216b
Hafiz Hanif <hafiz.hanif992@gmail.com>
2023-10-11 16:05:01 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -23,50 +23,50 @@ use App\Http\Controllers\DistinationController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
 
 Route::group(['middleware' => 'auth.custom'], function () {
     Route::get('/dashboard', [UserController::class, 'dashboard'])->name('dashboard');
     Route::get('/rides', [RideController::class, 'showRides'])->name('ride.index');
     Route::post('/rides/store', [RideController::class, 'store'])->name('ride.store');
-    
+
     Route::get('/hotels', [HotelController::class, 'show'])->name('hotels');
     // Route::get('/hotels/create', [HotelController::class, 'create'])->name('hotels.create');
     Route::post('/hotels/submit', [HotelController::class, 'store'])->name('hotels.store');
     Route::get('/hotels/delete/{hotelID}', [HotelController::class, 'delete'])->name('hotels.delete');
-    
+
     Route::get('/destination',[DistinationController::class, 'showDestination']);
     Route::post('/destination',[DistinationController::class, 'addEditDeleteDestination']);
 
     Route::get('/driver',[DriverController::class, 'index'])->name('driver.index');
     Route::post('/driver/store', [DriverController::class, 'store'])->name('drivers.store');
-    
-    
+
+
     Route::get('/permision', [PermissionController::class, 'showPermission']);
     Route::post('/permision', [PermissionController::class, 'addPermission']);
-    
+
     Route::get('/permision/{roleID}', [PermissionController::class, 'showPermissionList']);
-    
-    
+
+
     Route::get('/managers',[ManagerController::class, 'index'])->name('managers.index');
     Route::post('/managers',[ManagerController::class, 'store'])->name('managers.store');
-    
+
     Route::get('/commissions',[CommissionController::class, 'index'])->name('commissions.index');
     Route::post('/commissions',[CommissionController::class, 'store'])->name('commissions.store');
     Route::get('/get-commission-rate',[ CommissionController::class,'getCommissionRate']);
-    
-    
+
+
     Route::get('/payments',[PaymentController::class,'index'])->name('payment.index');
     // Route::post('/controller/payments',[PaymentController::class,'MyPayments'])->name('payment.controller.index');
-    
+
     Route::post('/add_payment/{driver}',[PaymentController::class,'addpay']);
     Route::post('/add_payment_hotel/{hotel}',[PaymentController::class,'addpayhotel']);
-    
+
 
     // CONTROLLER ROUTE
 
-    // Hotel 
+    // Hotel
     Route::post('controller/hotels',[HotelController::class,'myhotels'])->name('hotel.controller.index');
 
     // Ride
