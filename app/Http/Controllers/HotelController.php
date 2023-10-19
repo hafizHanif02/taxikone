88e2d89fa5250c07b879fda1aa6e4448ca8f2e90
Hafiz Hanif <hafiz.hanif992@gmail.com>
2023-10-18 18:57:40 -0700

    Update HotelController.php

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -17,7 +17,7 @@ class HotelController extends Controller
             $userData = Auth::user();
 
             if($userData->is_admin){
-                $hotels = hotel::with('manager', 'ridesCount')->get();
+                $hotels = hotel::with('manager')->withCount('rides')->get();
                 return $hotels;
                 $managers = User::where('is_controller',1)->get();
                 return view('admin.hotels', [
