b7fe9f30586687c559cf37224ac90588b920dace
hafizHanif02 <hafiz.hanif992@gmail.com>
2023-10-19 09:34:56 +0500

    Update DriverController.php

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -34,7 +34,7 @@ class DriverController extends Controller
                 foreach($driver as $dri){
                     $dri->rides = ride::where('driver_id', $dri->id)->count();
                 }
-                $driver->sortBy('rides');
+                $driver = $driver->sortBy('rides');
                 return view('admin.driver', ['userData'=>$userData,'drivers'=>$driver]);
             }
             return "asdfdsf";
